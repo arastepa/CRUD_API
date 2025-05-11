cf6473114fbf9c236a7b2996ed8927098eecf4c6
arastepa <stepanyan2298@gmail.com>
2025-05-11 17:11:17 +0400

    fix: eslint

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import http, { IncomingMessage, ServerResponse } from 'http';
+import {
+  getUsers,
+  getUser,
+  createUserHandler,
+  updateUserHandler,
+  deleteUserHandler,
+} from './controllers/userController';
+
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url || '';
+    const method = req.method || '';
+
+    if (url === '/api/users' && method === 'GET') {
+      getUsers(req, res);
+    } else if (url.startsWith('/api/users/') && method === 'GET') {
+      const userId = url.split('/')[3];
+      getUser(req, res, userId);
+    } else if (url === '/api/users' && method === 'POST') {
+      createUserHandler(req, res);
+    } else if (url.startsWith('/api/users/') && method === 'PUT') {
+      const userId = url.split('/')[3];
+      updateUserHandler(req, res, userId);
+    } else if (url.startsWith('/api/users/') && method === 'DELETE') {
+      const userId = url.split('/')[3];
+      deleteUserHandler(req, res, userId);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Endpoint not found' }));
+    }
+  }
+);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
