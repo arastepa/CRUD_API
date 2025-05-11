950e59d06c60c94de90f0d3a4abcde2106d00bdf
arastepa <stepanyan2298@gmail.com>
2025-05-12 00:53:34 +0400

    feat: add jest tests

--- src/app.ts
+++ src/app.ts
@@ -12,27 +12,32 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-const server = http.createServer(
+export const server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    const url = req.url || '';
-    const method = req.method || '';
+    try {
+      const url = req.url || '';
+      const method = req.method || '';
 
-    if (url === '/api/users' && method === 'GET') {
-      getUsers(req, res);
-    } else if (url.startsWith('/api/users/') && method === 'GET') {
-      const userId = url.split('/')[3];
-      getUser(req, res, userId);
-    } else if (url === '/api/users' && method === 'POST') {
-      createUserHandler(req, res);
-    } else if (url.startsWith('/api/users/') && method === 'PUT') {
-      const userId = url.split('/')[3];
-      updateUserHandler(req, res, userId);
-    } else if (url.startsWith('/api/users/') && method === 'DELETE') {
-      const userId = url.split('/')[3];
-      deleteUserHandler(req, res, userId);
-    } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Endpoint not found' }));
+      if (url === '/api/users' && method === 'GET') {
+        getUsers(req, res);
+      } else if (url.startsWith('/api/users/') && method === 'GET') {
+        const userId = url.split('/')[3];
+        getUser(req, res, userId);
+      } else if (url === '/api/users' && method === 'POST') {
+        createUserHandler(req, res);
+      } else if (url.startsWith('/api/users/') && method === 'PUT') {
+        const userId = url.split('/')[3];
+        updateUserHandler(req, res, userId);
+      } else if (url.startsWith('/api/users/') && method === 'DELETE') {
+        const userId = url.split('/')[3];
+        deleteUserHandler(req, res, userId);
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Endpoint not found' }));
+      }
+    } catch {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Internal Server Error' }));
     }
   }
 );
